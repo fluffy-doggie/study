42132a536dc0fad996df362a5d90b98705280913
Qilong Yuan <qilong_y@qq.com>
2018-06-06 19:33:13 +0800

    2018/06/06 19:33

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+/* 关于exports与module.exports的区别
+** 1. module.exports => {} <= exports
+** 2. require 返回的是 module.exports
+** 3. 不更改导出对象（仅添加属性方法等）则使用exports
+*/
+
+console.log(exports);
+console.log(module.exports);
+
+module.exports = {a: 'a'};
+
+console.log(exports);
+console.log(module.exports)
